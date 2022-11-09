e3219c2f059ada6af2a1a1953fef668e82ff9220
Riuck <tforest@gaming.tech>
2022-11-09 09:57:08 +0100

    maj 2.0

--- exolol.py
+++ exolol.py
@@ -1,4 +1,4 @@
-def input():
+(def input():
 
 #renvoie un caractere de type string au hasard
 
@@ -48,3 +48,77 @@ identite = nom + '' + prenom # retourne "Forest Tom"
 integerValue = 342 #retourne 342
 stringIntegerValue = str(342)
 mlg = [1,2,3,4,87,3,6,45,2]
+
+
+#Exercice 1 #Faire une fonction qui concatene 2 chaînes de charactères, les séparants par une virgule  
+#DEBUT
+def concatWithComma(strA, strB):
+   #je m'assure que strA soit bien de type str
+   stringifiedA = str(strA)
+   #je m'assure que strB soit bien de type str
+   stringifiedA = str(strB)
+   #retourner strA + ',' + strB
+   return strA + ',' + strB
+
+concatWithComma(23, "toto"):
+
+#FIN
+
+#Exercice 2 #Faire une fonction qui itere sur tous les index d'un tableau renvoyant une chaine de charactère #avec l'ensemble des occurences d'un chiffres e.g: #pour tableau = [0,1,1,1,0,1,1,0,1] #la fonction(tableau, 0) doit renvoyer "0,4,7" n'hésitez pas a implémenter la premiere fonction;)  
+tableau = [0,1,1,1,0,1,1,0,1]
+#def la fct findIndex qui itere sur tableau, cherchant l'index des diff occurence de x
+def finIndex(tableau, x):
+   #def i un index de départ
+   i = 0
+   #def chaineRetour telle qu'une chaine de caractere vide
+   chaineRetour = ''
+    #Définir le nombre d'élément du tableau
+   #tant que i est diff du nb d'elt dans le tableau
+      while (i != lent(tableau)):
+         #Alors j'attribue a une variable  la valeur de tableau a l'index i
+         selected = tableau[i]
+         #  je def un boolen tel que firstTurn est true
+         firstTurn = True
+         #si selected est égal a x et que firstTurn est true 
+         if selected == x and firstTurn == True :
+            #Alors on assigne a chainRetour le retour de str(i)
+            chainRetour = str(i)
+            #changer la valeur de firstTurn a false
+            firstTurn = False
+         #sinon si selected est egal a x
+         if selected == x :
+            #alors j'appelle concatWithComma tel que : concatWithComma(chaineRetour, i) à une chaineRetour
+            chaineRetour = concatWithComma(chaineRetour, i)
+            #j'incremente i de 1
+            i = i + 1
+#retourne la chaine de retour
+return chaineRetour
+
+
+
+
+
+
+
+
+#definir mon index
+i = 0 
+while (i != 0 len(tableau)):
+   selected = tableau[i]
+   i = i + 1
+
+
+
+
+
+
+
+
+
+#Exercice 3 #Renvoyer / Afficher un message
+#DEBUT
+
+msg=str(input("Entrez vôtre message : "))
+print(msg)
+
+#FIN)
--- test.py
+++ test.py
@@ -7,3 +7,22 @@ def minijeu(x):
         y=input("Devine la lettre")
     #lorsque y est égale a x alors
     print("réussi")
+
+
+#Exercice 1 #Faire une fonction qui concatene 2 chaînes de charactères, les séparants par une virgule  
+#DEBUT
+
+str1= "tomato"
+str2= "potate"
+print(str1, str2, sep=",")
+
+#FIN
+
+#Exercice 2 #Faire une fonction qui itere sur tous les index d'un tableau renvoyant une chaine de charactère #avec l'ensemble des occurences d'un chiffres e.g: #pour tableau = [0,1,1,1,0,1,1,0,1] #la fonction(tableau, 0) doit renvoyer "0,4,7" n'hésitez pas a implémenter la premiere fonction;)  
+#Exercice 3 #Renvoyer / Afficher un message
+#DEBUT
+
+msg=str(input("Entrez vôtre message : "))
+print(msg)
+
+#FIN
